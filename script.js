c5c26be64ea8f7ebaef9b88f28a5c50770a642eb
Narmin Murshudova <119409921+NarminM11@users.noreply.github.com>
2023-11-29 10:31:59 +0400

    Search Button

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-var row = document.querySelector(".loadProduct")
+var row = document.querySelector(".loadProduct");
 
 fetch('https://dummyjson.com/products')
     .then(response => response.json())
     .then(data => {
-         
         data.products.map(product => {
-            loadProducts(product);  
+            loadProducts(product);
         });
     })
     .catch(err => console.error(err));
 
 function loadProducts(data) {
-
-    row.innerHTML +=`
+    row.innerHTML += `
     <div class="blank">
-    <a href="details.html?id=${data.id}" class="btn">
-    <div class="cards">
-    <div class="product" > 
-        <img src="${data.images[0]}">
-        <h6 class="title">${data.title}</h6>
-        <div class="info"> <span class="text">${data.description}</span> </div>
-        <div class="price"> <span>${data.price} </span></div> 
-    </div>
-</div></a></div>
-    `
+      <a href="details.html?id=${data.id}" class="btn">
+        <div class="cards">
+          <div class="product" > 
+            <img src="${data.images[0]}">
+            <h6 class="title">${data.title}</h6>
+            <div class="info"> <span class="text">${data.description}</span> </div>
+            <div class="price"> <span>${data.price} </span></div> 
+          </div>
+        </div>
+      </a>
+    </div>`;
 }
 
+function searchProducts() {
+    var searchInput = document.getElementById("searchInput").value.toLowerCase();
+    row.innerHTML = ""; 
 
-
+    fetch('https://dummyjson.com/products')
+        .then(response => response.json())
+        .then(data => {
+            data.products.forEach(product => {
+                if (product.title.toLowerCase().includes(searchInput)) {
+                    loadProducts(product);
+                }
+            });
+        })
+        
+        .catch(err => console.error(err));
+}
