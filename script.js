ed80a7c8cd3be8d05fed05c6343371b57c8325ec
Narmin Murshudova <119409921+NarminM11@users.noreply.github.com>
2023-12-02 15:20:28 +0400

    pagination

--- script.js
+++ script.js
@@ -1,43 +1,72 @@
 var row = document.querySelector(".loadProduct");
+var currentPage = 1;
+var perPageProducts = 10;
 
-fetch('https://dummyjson.com/products')
-    .then(response => response.json())
-    .then(data => {
-        data.products.map(product => {
-            loadProducts(product);
-        });
-    })
-    .catch(err => console.error(err));
+fetch("https://dummyjson.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    displayProducts(data.products);
+  })
+  .catch((err) => console.error(err));
 
 function loadProducts(data) {
-    row.innerHTML += `
-    <div class="blank">
-      <a href="details.html?id=${data.id}" class="btn">
-        <div class="cards">
-          <div class="product" > 
-            <img src="${data.images[0]}">
-            <h6 class="title">${data.title}</h6>
-            <div class="info"> <span class="text">${data.description}</span> </div>
-            <div class="price"> <span>${data.price} </span></div> 
-          </div>
-        </div>
-      </a>
-    </div>`;
+  row.innerHTML += `
+        <div class="blank">
+          <a href="details.html?id=${data.id}" class="btn">
+            <div class="cards">
+              <div class="product" > 
+                <img src="${data.images[0]}">
+                <h6 class="title">${data.title}</h6>
+                <div class="info"> <span class="text">${data.description}</span> </div>
+                <div class="price"> <span>${data.price} </span></div> 
+              </div>
+            </div>
+          </a>
+        </div>`;
+}
+function searchProducts() {
+  var searchInput = document.getElementById("searchInput").value.toLowerCase();
+  row.innerHTML = "";
+
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      data.products.forEach((product) => {
+        if (product.title.toLowerCase().includes(searchInput)) {
+          loadProducts(product);
+        }
+      });
+    })
+
+    .catch((err) => console.error(err));
+}
+function displayProducts(products) {
+  var startingPosition = (currentPage - 1) * perPageProducts;
+  var endingPosition = startingPosition + perPageProducts;
+  var displayedProducts = products.slice(startingPosition, endingPosition);
+
+  row.innerHTML = "";
+  displayedProducts.forEach((product) => {
+    loadProducts(product);
+  });
 }
 
-function searchProducts() {
-    var searchInput = document.getElementById("searchInput").value.toLowerCase();
-    row.innerHTML = ""; 
+function fetchProducts() {
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      displayProducts(data.products);
+    })
+    .catch((err) => console.error(err));
+}
+function nextPage() {
+  currentPage++;
+  fetchProducts();
+}
 
-    fetch('https://dummyjson.com/products')
-        .then(response => response.json())
-        .then(data => {
-            data.products.forEach(product => {
-                if (product.title.toLowerCase().includes(searchInput)) {
-                    loadProducts(product);
-                }
-            });
-        })
-        
-        .catch(err => console.error(err));
+function previPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchProducts();
+  }
 }
