c8d28aad541755fef8fca3388d8ae9b49acd5de8
Narmin Murshudova <119409921+NarminM11@users.noreply.github.com>
2023-12-05 00:24:24 +0400

    category

--- details.js
+++ details.js
@@ -13,20 +13,22 @@ async function GetProductsById(id) {
 async function GetProductsData() {
   let product = await GetProductsById(id);
   console.log(product);
+
+  let galleryHTML = product.images.map((imageUrl) => {
+    return `<img src="${imageUrl}" class="gallery-image">`;
+  }).join('');
+
   productDetail.innerHTML += 
-  `<div class="blank">
-      <a href="details.html?id=${product.id}" class="btn">
-        <div class="cards">
-          <div class="product"> 
-            <img src="${product.images[0]}">
-            <h6 class="title">${product.title}</h6>
-            <div class="info"><span class="text">${product.description}</span></div>
-            <div class="price"><span>${product.price}</span></div> 
-          </div>
+    `<div class="blank">
+      <div class="cards">
+        <div class="product"> 
+          <div class="gallery">${galleryHTML}</div>
+          <h6 class="title">${product.title}</h6>
+          <div class="info"><span class="text">${product.description}</span></div>
+          <div class="price"><span>${product.price}</span></div> 
         </div>
-      </a>
-    </div>
-  `;
+      </div>
+    </div>`;
 }
 
 GetProductsData();
--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var row = document.querySelector(".loadProduct");
+var currentPage = 1;
+var productsPerPage = 10;
+var select = document.querySelector("#select_option");
+
+let productData;
+fetch("https://dummyjson.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    productData = data.products;
+
+    productData.map((product) => {
+      loadProducts(product);
+    });
+  })
+  .catch((err) => console.error());
 
 function loadProducts(data) {
-    row.innerHTML += `
+  row.innerHTML += `
         <div class="blank">
             <a href="details.html?id=${data.id}" class="btn">
                 <div class="cards">
@@ -18,29 +33,82 @@ function loadProducts(data) {
 }
 
 function searchProducts() {
-    var searchInput = document.getElementById("searchInput").value.toLowerCase();
-    row.innerHTML = "";
+  var searchInput = document.getElementById("searchInput").value.toLowerCase();
+  row.innerHTML = "";
 
-    fetch('https://dummyjson.com/products')
-        .then(response => response.json())
-        .then(data => {
-            data.products.forEach(product => {
-                if (product.title.toLowerCase().includes(searchInput)) {
-                    loadProducts(product);
-                }
-            });
-        })
-        .catch(err => console.error(err));
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      data.products.forEach((product) => {
+        if (product.title.toLowerCase().includes(searchInput)) {
+          loadProducts(product);
+        }
+      });
+    })
+    .catch((err) => console.error(err));
 }
 
-let submitBtn = document.getElementsByClassName('sbutton')[0];
-submitBtn.addEventListener('click', searchProducts);
+let submitBtn = document.getElementsByClassName("sbutton")[0];
+submitBtn.addEventListener("click", searchProducts);
 
-fetch('https://dummyjson.com/products')
-    .then(response => response.json())
-    .then(data => {
-        data.products.forEach(product => {
-            loadProducts(product);
-        });
+function displayProducts(products) {
+  var startIndex = (currentPage - 1) * productsPerPage;
+  var endIndex = startIndex + productsPerPage;
+  var currentProducts = products.slice(startIndex, endIndex);
+
+  row.innerHTML = "";
+  currentProducts.forEach((product) => {
+    loadProducts(product);
+  });
+}
+
+function fetchProducts() {
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      displayProducts(data.products);
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
+}
+function nextPage() {
+  currentPage++;
+  fetchProducts();
+}
+
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchProducts();
+  }
+}
+
+fetch("https://dummyjson.com/products/categories")
+  .then((response) => response.json())
+  .then((categorydata) => {
+    categorydata.map((item) => {
+      select.innerHTML += `
+        <option class="opt" value="${item}">${item}</option>
+        `;
+    });
+  })
+  .catch((err) => console.error(err));
+
+function getSelectedValue() {
+  var selectElement = document.getElementById("select_option");
+  var selectedValue = selectElement.value;
+  var filteredData = productData.filter(
+    (item) => item.category === selectedValue
+  );
+  row.innerHTML = "";
+  if (selectedValue === "all") {
+    productData.map((product) => {
+      loadProducts(product);
+    });
+  } else {
+    filteredData.map((product) => {
+      loadProducts(product);
+    });
+  }
+
+  console.log(selectedValue);
+}
